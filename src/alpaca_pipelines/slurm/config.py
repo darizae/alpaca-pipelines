582b2d377cdbd69e0c87d760a5e8bbc866b08401
darizae <danielarizae@gmail.com>
2026-03-02 14:39:50 -0500

    Fix path

--- src/alpaca_pipelines/slurm/config.py
+++ src/alpaca_pipelines/slurm/config.py
@@ -24,9 +24,7 @@ class SlurmConfig(BaseModel):
     error_pattern: str = "slurm-%j.err"
 
     conda_env: str | None = None
-    venv_path: str = (
-        "/projects/extern/kisski/kisski-alpaca-2/dir.project/repos/alpaca-pipelines/.venv"
-    )
+    venv_path: str = "/projects/extern/kisski/kisski-alpaca-2/dir.project/alpaca-pipelines/.venv"
     modules: list[str] = Field(default_factory=list)
 
     extra_sbatch_lines: list[str] = Field(default_factory=list)
